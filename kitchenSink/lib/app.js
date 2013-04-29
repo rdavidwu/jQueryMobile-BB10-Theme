7a7e3010f8bc0a5eb8fadac0fc88bc84062e7842
David Wu <rewu@rim.com>
2013-04-29 11:01:58 -0400

    Added application menu plugin. Added new application menu demo to kitchensink

--- kitchenSink/lib/app.js
+++ kitchenSink/lib/app.js
@@ -21,6 +21,9 @@ App.init = function () {
 	$("#actionBarSample").live("pageinit", function() {
 		App.page.actionBarSample.init();
 	});
+	$('#applicationMenu').live("pageinit", function() {
+		App.page.applicationMenu.init();
+	});
 }
 
 App.utils = {
@@ -38,7 +41,8 @@ App.page = {
 	progress: {},
 	slider: {},
 	toggle: {},
-	actionBarSample: {}
+	actionBarSample: {},
+	applicationMenu: {}
 }
 
 App.page.activity.init = function() {
@@ -193,7 +197,14 @@ App.page.actionBarSample.init = function() {
 	});
 }
 
-
+App.page.applicationMenu.init = function() {
+	if(typeof blackberry != 'undefined'){
+		blackberry.event.addEventListener('swipedown', function(){
+			$('#top').panel("open");
+		});
+		$('#menuBtn').css('display','none');
+	}
+}
 
 App.init();
 
